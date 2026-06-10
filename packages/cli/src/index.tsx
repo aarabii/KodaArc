201bb3f15c59ae428a84fad92bdf63cfcb31239b
aarabii <arv95464@gmail.com>
2026-06-10 13:35:11 +0530

    added keyboard layer

--- packages/cli/src/index.tsx
+++ packages/cli/src/index.tsx
@@ -3,25 +3,28 @@ import { createRoot } from "@opentui/react";
 import { Header } from "./components/header";
 import { InputBar } from "./components/inputBar";
 import { ToastProvider } from "./providers/toast";
+import { KeyboardLayerProvider } from "./providers/keyboardLayer";
 
 function App() {
   return (
-    <ToastProvider>
-      <box
-        alignItems="center"
-        justifyContent="center"
-        flexGrow={1}
-        backgroundColor="black"
-        width="100%"
-        height="100%"
-        gap={2}
-      >
-        <Header />
-        <box width="100%" maxWidth={78} paddingX={2}>
-          <InputBar onSubmit={() => {}} />
+    <KeyboardLayerProvider>
+      <ToastProvider>
+        <box
+          alignItems="center"
+          justifyContent="center"
+          flexGrow={1}
+          backgroundColor="black"
+          width="100%"
+          height="100%"
+          gap={2}
+        >
+          <Header />
+          <box width="100%" maxWidth={78} paddingX={2}>
+            <InputBar onSubmit={() => {}} />
+          </box>
         </box>
-      </box>
-    </ToastProvider>
+      </ToastProvider>
+    </KeyboardLayerProvider>
   );
 }
 
