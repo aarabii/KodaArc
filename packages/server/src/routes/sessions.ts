8a0bdfed511e5262cc0d22dbfbe24057f7259cb0
aarabii <arv95464@gmail.com>
2026-06-12 22:41:50 +0530

    removed unused lib

--- packages/server/src/routes/sessions.ts
+++ packages/server/src/routes/sessions.ts
@@ -1,5 +1,4 @@
 import { Hono } from "hono";
-import { HTTPException } from "hono/http-exception";
 import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
 import { db } from "@koda-arc/database";
